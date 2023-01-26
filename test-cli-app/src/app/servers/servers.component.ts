82b449faccce4da43dfa074c1394c1aa1d72ba07
GokulRG <ramakrishnan.go@husky.neu.edu>
2023-01-26 03:24:22 -0800

    Basics part 2

--- test-cli-app/src/app/servers/servers.component.ts
+++ test-cli-app/src/app/servers/servers.component.ts
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent {
+  allowNewServers = false;
+  serverCreationStatus = "No server was created!";
+  latestServerName = '';
 
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServers = true;
+    }, 3000);
+  }
+
+  onAddServerClicked() {
+    this.serverCreationStatus = `New Server Created with name ${this.latestServerName}!`;
+    const newServer = document.createElement("app-server");
+    //newServer.innerHTML = '<app-server></app-server>';
+    let serverContainer = document.querySelector('.servers-container');
+    serverContainer?.appendChild(newServer);
+    setTimeout(() => {
+      this.serverCreationStatus = "No server was created!";
+      const inputElement = document.getElementById('server-name') as HTMLInputElement;
+      inputElement!.value = "";
+    }, 2000);
+  }
+
+  // onServerNameChange(event: any) {
+  //   this.latestServerName = event?.target?.value;
+  // }
 }
--- test-cli-app/src/app/success-alert/success-alert.component.ts
+++ test-cli-app/src/app/success-alert/success-alert.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./success-alert.component.css']
 })
 export class SuccessAlertComponent {
+  username='';
 
+  resetUsername() {
+    this.username = '';
+  }
 }
